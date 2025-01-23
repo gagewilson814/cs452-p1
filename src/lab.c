15037d49c9b4ecb4e04650bbcb4ec36d85464ece
gagewilson814 <gagewilson814@u.boisestate.edu>
2025-01-23 15:27:19 -0700

    fix formatting

--- src/lab.c
+++ src/lab.c
@@ -1,11 +1,10 @@
 #include "lab.h"
 
-list_t *list_init(void (*destroy_data)(void *), int (*compare_to)(const void *, const void *))
-{
+list_t *list_init(void (*destroy_data)(void *),
+                  int (*compare_to)(const void *, const void *)) {
 
   list_t *list = (list_t *)malloc(sizeof(list_t));
-  if (!list)
-  {
+  if (!list) {
     return NULL;
   }
 
@@ -14,8 +13,7 @@ list_t *list_init(void (*destroy_data)(void *), int (*compare_to)(const void *, 
   list->size = 0;
 
   list->head = (node_t *)malloc(sizeof(node_t));
-  if (!list->head)
-  {
+  if (!list->head) {
     free(list);
     return NULL;
   }
@@ -27,17 +25,14 @@ list_t *list_init(void (*destroy_data)(void *), int (*compare_to)(const void *, 
   return list;
 }
 
-void list_destroy(list_t **list)
-{
-  if (!list || !*list)
-  {
+void list_destroy(list_t **list) {
+  if (!list || !*list) {
     return;
   }
 
   list_t *l = *list;
   node_t *curr = l->head->next;
-  while (curr != l->head)
-  {
+  while (curr != l->head) {
     node_t *tmp = curr;
     curr = curr->next;
     l->destroy_data(tmp->data);
@@ -49,10 +44,8 @@ void list_destroy(list_t **list)
   *list = NULL;
 }
 
-list_t *list_add(list_t *list, void *data)
-{
-  if (!list)
-  {
+list_t *list_add(list_t *list, void *data) {
+  if (!list) {
     return NULL;
   }
   node_t *newNode = (node_t *)malloc(sizeof(node_t));
@@ -81,18 +74,14 @@ void *list_remove_index(list_t *list, size_t index) {
   return data;
 }
 
-int list_indexof(list_t *list, void *data)
-{
-  if (!list)
-  {
+int list_indexof(list_t *list, void *data) {
+  if (!list) {
     return -1;
   }
   int currentIndex = 0;
   node_t *curr = list->head->next;
-  while (curr != list->head)
-  {
-    if (list->compare_to(curr->data, data) == 0)
-    {
+  while (curr != list->head) {
+    if (list->compare_to(curr->data, data) == 0) {
       return currentIndex;
     }
     curr = curr->next;
